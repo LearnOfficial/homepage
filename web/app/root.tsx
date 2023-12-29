fbfa031b38b7ff7fddbe233c56d6cd2e826587c5
camilo-camargo <camilocamargo49@gmail.com>
2023-12-29 15:23:29 -0500

    Add tailwindcss.

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,4 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
+import tailwindcssStylesHref from "./tailwind.css";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -10,7 +10,7 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(tailwindcssStylesHref ? [{ rel: "stylesheet", href: tailwindcssStylesHref }] : []),
 ];
 
 export default function App() {
